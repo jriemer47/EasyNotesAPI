c7c702785fec86e58b3668a349323b64065f6b9d
Jonathan Riemer <jriemer@Jonathans-MacBook-Pro-2.local>
2018-02-27 17:41:59 -0800

    added multiple folder/files, finished functions

---
+++ node-easy-notes-app/app/routes/note.routes.js
@@ -0,0 +1,15 @@
+module.exports = function(app) {
+  var notes = require('../controllers/note.controller.js');
+
+  // create a new note
+  app.post('/notes', notes.create);
+
+  // retrieve all notes
+  app.get('/notes', notes.findAll);
+
+  // retrieve a single note with noteId
+  app.get('/notes/:noteId', notes.update);
+
+  // delete a note with noteId
+  app.delete('/notes/:noteId', notes.delete);
+}
--- node-easy-notes-app/server.js
+++ node-easy-notes-app/server.js
@@ -10,12 +10,36 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 // parse request of content-type - application/json
 app.use(bodyParser.json())
+// configuring the database
+var dbConfig = require('./config/database.config.js');
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.url, {
+
+});
+
+mongoose.connection.on('error', function() {
+  console.log('Could not connect to the database. Exiting now...');
+  process.exit();
+});
+
+mongoose.connection.once('open', function() {
+  console.log("Successfully connect to the database");
+});
+
 
 // define a simple route
 app.get('/', function(req, res) {
   res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
 });
 
+
+// .........
+// require notes routes
+require('./app/routes/note.routes.js')(app);
+// .........
 // listen for requests
 app.listen(3000, function() {
   console.log("Server is listening on port 3000");
